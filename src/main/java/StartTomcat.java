a0743e14beae61769f54e84adf7ec906dc3e6fc0
Ralph Schaer <ralphschaer@gmail.com>
2012-06-21 06:27:29 +0200

    prepare wro4j test

--- src/main/java/StartTomcat.java
+++ src/main/java/StartTomcat.java
@@ -2,6 +2,7 @@ import ch.ralscha.embeddedtc.EmbeddedTomcat;
 
 public class StartTomcat {
 	public static void main(final String[] args) throws Exception {
-		EmbeddedTomcat.create().addContextEnvironmentAndResourceFromFile("./src/main/config/tomcat.xml").startAndWait();
+		EmbeddedTomcat.create().setContextPath("/e4")
+				.addContextEnvironmentAndResourceFromFile("./src/main/config/tomcat.xml").startAndWait();
 	}
 }
